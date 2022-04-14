57fd80bbd76509f287ca925564ed9cd2d1ee40d2
jsrankin-com <admin@jsrankin.com>
2022-04-14 07:49:38 -0400

    CRUD is now working

---
+++ PasswordManager/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager
+{
+    class Account
+    {
+        public string description { get; set; }
+        public string userid { get; set; }
+        public string loginurl { get; set; }
+        public string accountnum { get; set; }
+        public string password { get; set; }
+    }
+}
